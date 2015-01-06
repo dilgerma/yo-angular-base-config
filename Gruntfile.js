07407db39addfcb8191994b91c6a996d66270622
dilgerm <martin@effectivetrainings.de>
2015-01-06 20:17:35 +0100

    dont use cdnify

--- Gruntfile.js
+++ Gruntfile.js
@@ -287,13 +287,7 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
+    // Copies remaining files to places other tasks can use
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -387,7 +381,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'filerev',
